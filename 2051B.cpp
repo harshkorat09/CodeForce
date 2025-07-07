55d7b1af94efa9abb1895a9925f471f9d9507534
harshkorat09 <harshkorat106@gmail.com>
2025-07-07 20:47:16 +0530

    Initial commit or update

--- 2051B.cpp
+++ 2051B.cpp
@@ -9,12 +9,14 @@ int main() {
         int n, a, b, c;
         cin >> n >> a >> b >> c;
 
-        int days = 0;
-        int total = 0;
         int pattern[3] = {a, b, c};
+        int cycle_sum = a + b + c;
+        int full_cycles = n / cycle_sum;
+        int days = full_cycles * 3;
+        int remaining = n - full_cycles * cycle_sum;
 
-        while (total < n) {
-            total += pattern[days % 3];
+        for (int i = 0; remaining > 0; i++) {
+            remaining -= pattern[i % 3];
             days++;
         }
 
